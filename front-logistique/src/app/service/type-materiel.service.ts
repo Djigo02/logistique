ccd864b4226d1c61deb7e3ee5d1fe11f534e9a28
djigo02 <mehmetdjigo@gmail.com>
2024-06-10 01:11:51 +0200

    Supression du dossier serv

--- front-logistique/src/app/service/type-materiel.service.ts
+++ front-logistique/src/app/service/type-materiel.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TypeMateriel } from '../model/type-materiel';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TypeMaterielService {
-//Normal
-  constructor(private httpClient: HttpClient) { }
+  //Normal
+  constructor(private httpClient: HttpClient) {}
 
-  apiUrl:string = 'http://localhost:8080/api/typesmateriels/';
+  apiUrl: string = 'http://localhost:8080/api/typesmateriels/';
 
   httpOptions = {
-    headers : new HttpHeaders({
-      'Content-Type' : 'application/json',
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
     }),
   };
 
-  getTypeMateriel(){
+  getTypeMateriel() {
     return this.httpClient.get(this.apiUrl);
   }
 
@@ -26,16 +26,21 @@ export class TypeMaterielService {
     return this.httpClient.get<TypeMateriel[]>(this.apiUrl);
   }
 
-  insertTypeMateriel(typeMateriel : TypeMateriel){
-    return this.httpClient.post(this.apiUrl,typeMateriel);
+  insertTypeMateriel(typeMateriel: TypeMateriel) {
+    return this.httpClient.post(this.apiUrl, typeMateriel);
   }
-  deleteTypeMateriel(id : number){
-    return this.httpClient.delete(this.apiUrl+id);
+
+  deleteTypeMateriel(id: number) {
+    return this.httpClient.delete(this.apiUrl + id);
   }
-  updateTypeMateriel(typeMateriel : TypeMateriel,id : number){
-    return this.httpClient.put(this.apiUrl+id,JSON.stringify(typeMateriel),this.httpOptions);
+  updateTypeMateriel(typeMateriel: TypeMateriel, id: number) {
+    return this.httpClient.put(
+      this.apiUrl + id,
+      JSON.stringify(typeMateriel),
+      this.httpOptions
+    );
   }
-  getTypeMaterielById(id:number){
-    return this.httpClient.get(this.apiUrl+id)
+  getTypeMaterielById(id: number) {
+    return this.httpClient.get(this.apiUrl + id);
   }
 }
