cb7de80cef3af8c66bfa75b9b52bf0cc6657e599
djigo02 <mehmetdjigo@gmail.com>
2024-07-02 15:03:09 +0200

    Sauvegarde 13h 02/01/2024

--- front-logistique/src/app/component/campus/lister-campus/lister-campus.component.ts
+++ front-logistique/src/app/component/campus/lister-campus/lister-campus.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {CampusService} from "../../../service/campus.service";
+import {Campus} from "../../../model/campus";
 
 @Component({
   selector: 'app-lister-campus',
   templateUrl: './lister-campus.component.html',
   styleUrls: ['./lister-campus.component.css']
 })
-export class ListerCampusComponent {
+export class ListerCampusComponent implements OnInit{
 
+  campusList!: Campus[];
+
+  ngOnInit() {
+    this.getCampus();
+  }
+
+  constructor(private router:Router, private campusService:CampusService) {
+  }
+
+  getCampus(){
+    this.campusService.getCampus().subscribe(data => {
+      this.campusList = data;
+      console.log(this.campusList);
+    });
+  }
+
+  goToCampusForm(){
+    this.router.navigate(['/admin/campus']);
+  }
 }
