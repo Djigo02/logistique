7605408fad21058b2f59537240e79594f2ba1ee6
djigo02 <mehmetdjigo@gmail.com>
2024-07-05 01:00:02 +0200

    Modification du sidebar avec les redirection

--- front-logistique/src/app/pages/navbar-admin/navbar-admin.component.ts
+++ front-logistique/src/app/pages/navbar-admin/navbar-admin.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar-admin',
   templateUrl: './navbar-admin.component.html',
   styleUrls: ['./navbar-admin.component.css']
 })
-export class NavbarAdminComponent {
+export class NavbarAdminComponent implements OnInit {
+  constructor(private router:Router){}
 
+  ngOnInit(){
+
+  }
+
+  // Aller au tableau de bord
+  goToDashboard(){
+    this.router.navigate(['/admin/dashboard']);
+  }
 }
