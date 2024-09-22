e5bf74322a406c0322cb29c518f8953b84404e61
Mouhamad DJIGO <mehmetdjigo@gmail.com>
2024-09-22 23:27:42 +0000

    Tableau de bord avec Repartitions des affectations par materiel

--- front-logistique/src/app/pages/login/login.component.ts
+++ front-logistique/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {AuthService} from "../../service/auth.service";
 import {Router} from "@angular/router";
 import {User} from "../../model/user";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-login',
@@ -13,11 +14,11 @@ export class LoginComponent  {
   email: string = '';
   password: string = '';
 
-  constructor(private authService : AuthService,private router:Router) {
+  constructor(private authService : AuthService,private router:Router, private notif :ToastrService) {
   }
   onSubmit() {
     this.authService.login(this.email, this.password).subscribe(res => {
-        alert(this.email + this.password);
+
         // Stocker le token reçu après connexion
         sessionStorage.setItem('access_token',res.access_token);
         this.authService.setToken(res.access_token);  // Stocke le token dans localStorage ou sessionStorage
@@ -28,9 +29,11 @@ export class LoginComponent  {
         // Récupère les informations utilisateur
         const user = this.authService.getUser();
         console.log('Utilisateur connecté :', user);
+        this.notif.success("Connexion reussie", "Connexion...");
         this.router.navigate(['/admin/dashboard']);
       },
       (err) => {
+      this.notif.error("Email ou mot de passe incorrect", "Erreur lors de la connexion");
         console.error('Échec de la connexion :', err);
       });
   }
